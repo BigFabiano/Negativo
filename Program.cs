18629158c5c8688fdfe3ced698f0493af63a1c4d
FabianoEtec <nbscbs@gmail.com>
2021-04-11 01:37:57 -0300

    Número Negativo finalizado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("\n -- N E G A T I V O -- ");
+            Console.Write("Digite um número: ");
+            double numero = Convert.ToDouble(Console.ReadLine());
+            
+            if (numero < 0)
+            {
+                Console.WriteLine($"Você digitou um número negativo. {numero}");
+            }
+            else
+            {
+                Environment.Exit(-1);
+            }
+        }
+    }
+}
